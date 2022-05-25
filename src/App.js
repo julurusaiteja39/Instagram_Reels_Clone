ed3104e1e5ccffc4d88c3c7b919c936999e4be88
Juluru Saiteja <60264167+julurusaiteja39@users.noreply.github.com>
2022-05-25 09:51:58 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import VideoCard from "./VideoCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hai</h1>
+      <div className="app__top">
+        <img className="app__logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
+          alt=""
+        />
+        <h1>Reels</h1>
+      </div>
+      <div className="app__videos">
+          <VideoCard 
+            channel='saiteja'
+            avatarSrc='https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4'
+            song='Test song - ssssangha'
+            url='https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4'
+            likes={950}
+            shares={30}
+          />
+          <VideoCard 
+            
+          />
+          <VideoCard />
+          <VideoCard />
+      </div>
     </div>
   );
 }
---
+++ src/VideoFooter.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './VideoFooter.css'
+import {Button,Avatar} from "@material-ui/core";
+function VideoFooter({channel,song,likes,shares,avatarSrc}) {
+    return (
+        <div className='videoFooter'>
+            <div className="videoFooter__text">
+                <Avatar src={avatarSrc} />
+                <h3>
+                    {channel} . <Button>Follow</Button>
+                </h3>
+            </div>
+        </div>
+    )
+}
+
+export default VideoFooter
